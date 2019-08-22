9f3152d54e23d9edc9cf4f2a73a3957411ee3030
lokin135 <lokin135help@gmail.com>
2019-08-22 14:00:26 +0300

    New GUI component: Space

---
+++ src/LokEngine/GUI/GUIObjects/GUISpace.java
@@ -0,0 +1,18 @@
+package LokEngine.GUI.GUIObjects;
+
+import LokEngine.Tools.Utilities.Vector2i;
+
+public class GUISpace extends GUIObject {
+
+    public GUISpace(Vector2i position, Vector2i size) {
+        super(position, size);
+    }
+
+    public GUISpace( Vector2i size) {
+        this(new Vector2i(),size);
+    }
+
+    public GUISpace() {
+        this(new Vector2i(),new Vector2i());
+    }
+}
