8150bd05a2da5104a89f0c57a2ac8bd8161de6b2
lokin135 <lokin135help@gmail.com>
2019-08-24 01:57:05 +0300

    New method for buffer loader

--- src/LokEngine/Loaders/BufferLoader.java
+++ src/LokEngine/Loaders/BufferLoader.java
@@ -6,6 +6,7 @@ import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL15;
 
 import java.nio.FloatBuffer;
+import java.util.ArrayList;
 
 public class BufferLoader {
 
@@ -15,6 +16,32 @@ public class BufferLoader {
 
             FloatBuffer pointsFB = BufferUtils.createFloatBuffer(points.length);
             pointsFB.put(points);
+            pointsFB.flip();
+
+            buffer = GL15.glGenBuffers();
+            GL15.glBindBuffer(GL15.GL_ARRAY_BUFFER, buffer);
+            GL15.glBufferData(GL15.GL_ARRAY_BUFFER, pointsFB, GL15.GL_STATIC_DRAW);
+
+        } catch (Exception e) {
+            Logger.error("Fail generate buffer!", "LokEngine_BufferLoader");
+            Logger.printException(e);
+        }
+        return buffer;
+    }
+
+    public static int load(ArrayList<Float> points) {
+        int buffer = -1;
+        try {
+
+            FloatBuffer pointsFB = BufferUtils.createFloatBuffer(points.size());
+
+            float[] arrayPoints = new float[points.size()];
+
+            for (int i = 0; i < arrayPoints.length; i++){
+                arrayPoints[i] = points.get(i);
+            }
+
+            pointsFB.put(arrayPoints);
             pointsFB.flip();
 
             buffer = GL15.glGenBuffers();
