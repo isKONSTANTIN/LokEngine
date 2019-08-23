1dc545999b15950826847450f7b922d598197a87
lokin135 <lokin135help@gmail.com>
2019-08-24 01:58:13 +0300

    New feature for sprite loader: pre-size

--- src/LokEngine/Loaders/SpriteLoader.java
+++ src/LokEngine/Loaders/SpriteLoader.java
@@ -9,32 +9,27 @@ import java.io.IOException;
 
 public class SpriteLoader {
 
+    public static Sprite loadSprite(String texturePath, float vertexSize, Shader shader) throws IOException {
+        Texture tex = TextureLoader.loadTexture(texturePath);
+
+        int vertexBuffer = BufferLoader.load(new float[]
+                {
+                        -tex.sizeX * vertexSize / 2f * 0.000520833f, -tex.sizeY * vertexSize / 2f * 0.000520833f,
+                        -tex.sizeX * vertexSize / 2f * 0.000520833f, tex.sizeY * vertexSize / 2f * 0.000520833f,
+                        tex.sizeX * vertexSize / 2f * 0.000520833f, tex.sizeY * vertexSize / 2f * 0.000520833f,
+                        tex.sizeX * vertexSize / 2f * 0.000520833f, -tex.sizeY * vertexSize / 2f * 0.000520833f
+                }
+        );
+
+        return new Sprite(tex,vertexBuffer,DefaultFields.defaultUVBuffer,1, shader);
+    }
+
+    public static Sprite loadSprite(String texturePath, float vertexSize) throws IOException {
+        return SpriteLoader.loadSprite(texturePath, vertexSize, DefaultFields.defaultShader);
+    }
+
     public static Sprite loadSprite(String texturePath) throws IOException {
-        Texture tex = TextureLoader.loadTexture(texturePath);
-
-        int vertexBuffer = BufferLoader.load(new float[]
-                {
-                        -tex.sizeX / 2f * 0.000520833f, -tex.sizeY / 2f * 0.000520833f,
-                        -tex.sizeX / 2f * 0.000520833f, tex.sizeY / 2f * 0.000520833f,
-                        tex.sizeX / 2f * 0.000520833f, tex.sizeY / 2f * 0.000520833f,
-                        tex.sizeX / 2f * 0.000520833f, -tex.sizeY / 2f * 0.000520833f
-                }
-        );
-
-        return new Sprite(tex,vertexBuffer,DefaultFields.defaultUVBuffer,1, DefaultFields.defaultShader);
+        return SpriteLoader.loadSprite(texturePath, 1, DefaultFields.defaultShader);
     }
 
-    public static Sprite loadSprite(String texturePath, Shader shader) throws IOException {
-        Texture tex = TextureLoader.loadTexture(texturePath);
-
-        int vertexBuffer = BufferLoader.load(new float[]
-                {
-                        -tex.sizeX / 2f * 0.000520833f, -tex.sizeY / 2f * 0.000520833f,
-                        -tex.sizeX / 2f * 0.000520833f, tex.sizeY / 2f * 0.000520833f,
-                        tex.sizeX / 2f * 0.000520833f, tex.sizeY / 2f * 0.000520833f,
-                        tex.sizeX / 2f * 0.000520833f, -tex.sizeY / 2f * 0.000520833f
-                }
-        );
-        return new Sprite(tex,vertexBuffer,DefaultFields.defaultUVBuffer,1,shader);
-    }
 }
