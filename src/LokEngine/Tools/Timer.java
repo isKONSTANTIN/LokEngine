6d53663cb3249cc02c9ace32607daffb4e8c93be
lokin135 <lokin135help@gmail.com>
2019-08-30 16:33:44 +0300

    New tool: Timer

---
+++ src/LokEngine/Tools/Timer.java
@@ -0,0 +1,32 @@
+package LokEngine.Tools;
+
+public class Timer {
+
+    private long startTime;
+    private long duration;
+
+    public Timer(){
+        resetTimer();
+    }
+
+    public boolean checkTime(){
+        return startTime - System.nanoTime() >= duration;
+    }
+
+    public void resetTimer(){
+        startTime = System.nanoTime();
+    }
+
+    public Timer setDurationInSeconds(float seconds){
+        return setDurationInMilliseconds((long)(seconds * 1000L));
+    }
+
+    public Timer setDurationInMilliseconds(long milliseconds){
+        return setDurationInNanoseconds(milliseconds * 1000000L);
+    }
+
+    public Timer setDurationInNanoseconds(long nanoseconds){
+        duration = nanoseconds;
+        return this;
+    }
+}
