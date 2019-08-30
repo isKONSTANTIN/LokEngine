1636d2d40f625094222d003bcc9aeec9a4ea0b8e
lokin135 <lokin135help@gmail.com>
2019-08-30 22:50:48 +0300

    Sprite: load and save methods

--- src/LokEngine/Components/AdditionalObjects/Sprite.java
+++ src/LokEngine/Components/AdditionalObjects/Sprite.java
@@ -1,16 +1,20 @@
 package LokEngine.Components.AdditionalObjects;
 
+import LokEngine.Loaders.SpriteLoader;
 import LokEngine.Render.Shader;
 import LokEngine.Render.Texture;
 import LokEngine.Tools.DefaultFields;
+import LokEngine.Tools.SaveWorker.Saveable;
 
-public class Sprite {
+public class Sprite implements Saveable {
 
     public Shader shader;
     public Texture texture;
     public int vertexBuffer;
     public int uvBuffer;
     public double size;
+
+    public Sprite(){ }
 
     public Sprite(Texture texture, int vertexBuffer, int uvBuffer){
         this.texture = texture;
@@ -43,5 +47,24 @@ public class Sprite {
                 objs.vertexBuffer == vertexBuffer &&
                 objs.uvBuffer == uvBuffer &&
                 objs.size == size;
+    }
+
+    @Override
+    public String save() {
+        return shader.save() + "\n" + texture.save() + "\n" + size + "\n" + SpriteLoader.getVertexSizes(this);
+    }
+
+    @Override
+    public Saveable load(String savedString) {
+        String[] data = savedString.split(System.getProperty("line.separator"));
+        Sprite loadedSprite = SpriteLoader.loadSprite((Texture)new Texture().load(data[1]), Float.valueOf(data[3]), (Shader)new Shader().load(data[0]));
+
+        this.size = Double.valueOf(data[2]);
+        this.texture = loadedSprite.texture;
+        this.shader = loadedSprite.shader;
+        this.vertexBuffer = loadedSprite.vertexBuffer;
+        this.uvBuffer = loadedSprite.uvBuffer;
+
+        return this;
     }
 }
