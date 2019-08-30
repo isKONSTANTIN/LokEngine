8605adb87345191a296d9c4bf2b765a522f3a86d
lokin135 <lokin135help@gmail.com>
2019-08-30 22:50:21 +0300

    Shader: load and save methods

--- src/LokEngine/Render/Shader.java
+++ src/LokEngine/Render/Shader.java
@@ -1,8 +1,11 @@
 package LokEngine.Render;
 
+import LokEngine.Loaders.ShaderLoader;
+import LokEngine.Tools.Logger;
+import LokEngine.Tools.SaveWorker.Saveable;
 import org.lwjgl.opengl.ARBShaderObjects;
 
-public class Shader {
+public class Shader implements Saveable {
 
     public int program;
     public Shader(int program){
@@ -10,6 +13,8 @@ public class Shader {
     }
 
     public static Shader currentShader;
+
+    public Shader(){ }
 
     public boolean equals(Object obj){
         Shader objs = (Shader)obj;
@@ -24,5 +29,25 @@ public class Shader {
     public static void unUse(){
         ARBShaderObjects.glUseProgramObjectARB(0);
         currentShader = null;
+    }
+
+    @Override
+    public String save() {
+        String[] patches = ShaderLoader.getPatches(this);
+
+        return patches[0] + "\n" + patches[1];
+    }
+
+    @Override
+    public Saveable load(String savedString) {
+        String[] patches = savedString.split(System.getProperty("line.separator"));
+
+        try {
+            this.program = ShaderLoader.loadShader(patches[0],patches[1]).program;
+        } catch (Exception e) {
+            Logger.warning("Fail load shader from save!","LokEngine_Shader");
+            Logger.printException(e);
+        }
+        return this;
     }
 }
