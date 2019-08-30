b6a605cb5bdb2505f41200bf4f4447fcf324126a
lokin135 <lokin135help@gmail.com>
2019-08-30 22:50:34 +0300

    Texture: load and save methods

--- src/LokEngine/Render/Texture.java
+++ src/LokEngine/Render/Texture.java
@@ -1,10 +1,18 @@
 package LokEngine.Render;
 
-public class Texture {
+import LokEngine.Loaders.TextureLoader;
+import LokEngine.Tools.Logger;
+import LokEngine.Tools.SaveWorker.Saveable;
+
+import java.io.IOException;
+
+public class Texture implements Saveable {
 
     public int buffer;
     public int sizeX;
     public int sizeY;
+
+    public Texture(){}
 
     public Texture(int buffer, int sizeX, int sizeY){
         this.buffer = buffer;
@@ -18,5 +26,25 @@ public class Texture {
         return objt.buffer == buffer &&
                 objt.sizeX == sizeX &&
                 objt.sizeY == sizeY;
+    }
+
+    @Override
+    public String save() {
+        return TextureLoader.getPath(this);
+    }
+
+    @Override
+    public Saveable load(String savedString) {
+        try {
+            Texture loadedTexture = TextureLoader.loadTexture(savedString);
+            this.buffer = loadedTexture.buffer;
+            this.sizeX = loadedTexture.sizeX;
+            this.sizeY = loadedTexture.sizeY;
+        } catch (IOException e) {
+            Logger.warning("Fail load texture from save!","LokEngine_Texture");
+            Logger.printException(e);
+        }
+
+        return this;
     }
 }
