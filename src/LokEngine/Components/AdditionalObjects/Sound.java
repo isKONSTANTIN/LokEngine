3d760dd8adb2e7fca2db8283340acda835114228
lokin135 <lokin135help@gmail.com>
2019-08-31 16:34:58 +0300

    Sound: load and save methods

--- src/LokEngine/Components/AdditionalObjects/Sound.java
+++ src/LokEngine/Components/AdditionalObjects/Sound.java
@@ -1,7 +1,21 @@
 package LokEngine.Components.AdditionalObjects;
 
-public class Sound {
+import LokEngine.Loaders.SoundLoader;
+import LokEngine.Tools.SaveWorker.Saveable;
+import LokEngine.Tools.Utilities.SoundType;
+
+public class Sound implements Saveable {
 
     public int buffer;
 
+    @Override
+    public String save() {
+        return SoundLoader.getPath(this);
+    }
+
+    @Override
+    public Saveable load(String savedString) {
+        buffer = SoundLoader.loadSound(savedString, SoundType.WAV).buffer;
+        return this;
+    }
 }
