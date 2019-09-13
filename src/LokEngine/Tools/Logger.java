0660b667161d89e3ff7d603ce7c4e285274b46b5
lokin135 <lokin135help@gmail.com>
2019-09-13 21:53:23 +0300

    Logger: add opportunity change colors and disable they

--- src/LokEngine/Tools/Logger.java
+++ src/LokEngine/Tools/Logger.java
@@ -12,70 +12,109 @@ public class Logger {
     public static boolean errorMessages = true;
     public static boolean exceptionMessages = true;
 
-    public static final String ANSI_RESET = "\u001B[0m";
-    public static final String ANSI_BLACK = "\u001B[30m";
-    public static final String ANSI_RED = "\u001B[31m";
-    public static final String ANSI_GREEN = "\u001B[32m";
-    public static final String ANSI_YELLOW = "\u001B[33m";
-    public static final String ANSI_BLUE = "\u001B[34m";
-    public static final String ANSI_PURPLE = "\u001B[35m";
-    public static final String ANSI_CYAN = "\u001B[36m";
-    public static final String ANSI_WHITE = "\u001B[37m";
+    public static boolean useColors = true;
+
+    public static String ANSIReset =       "\u001B[0m";
+    public static String ANSIBlackColor =  "\u001B[30m";
+    public static String ANSIRedColor =    "\u001B[31m";
+    public static String ANSIGreenColor =  "\u001B[32m";
+    public static String ANSIYellowColor = "\u001B[33m";
+    public static String ANSIBlueColor =   "\u001B[34m";
+    public static String ANSIPurpleColor = "\u001B[35m";
+    public static String ANSICyanColor =   "\u001B[36m";
+    public static String ANSIWhiteColor =  "\u001B[37m";
 
     private static int lastMessageType = 0;
 
     public static void debug(Object message){
         if (debugMessages){
-            System.out.println(ANSI_CYAN + "Debug: " + message + ANSI_RESET);
+            if (useColors){
+                System.out.println(ANSICyanColor + "Debug: " + message + ANSIReset);
+            }else{
+                System.out.println("Debug: " + message);
+            }
+
             lastMessageType = 1;
         }
     }
 
     public static void debug(Object message, String tag){
         if (debugMessages){
-            System.out.println(ANSI_CYAN + "Debug." + tag + ": " + message + ANSI_RESET);
+            if (useColors) {
+                System.out.println(ANSICyanColor + "Debug." + tag + ": " + message + ANSIReset);
+            }else{
+                System.out.println("Debug." + tag + ": " + message);
+            }
             lastMessageType = 1;
         }
     }
 
     public static void info(Object message){
         if (infoMessages){
-            System.out.println(ANSI_WHITE + "Info: " + message + ANSI_RESET);
+            if (useColors){
+                System.out.println(ANSIWhiteColor + "Info: " + message + ANSIReset);
+            }else{
+                System.out.println("Info: " + message);
+            }
+
             lastMessageType = 2;
         }
     }
 
     public static void info(Object message, String tag){
         if (infoMessages){
-            System.out.println(ANSI_WHITE + "Info." + tag + ": " + message + ANSI_RESET);
+            if (useColors) {
+                System.out.println(ANSIWhiteColor + "Info." + tag + ": " + message + ANSIReset);
+            }else{
+                System.out.println("Info." + tag + ": " + message);
+            }
             lastMessageType = 2;
         }
     }
 
     public static void warning(Object message){
         if (warningMessages){
-            System.out.println(ANSI_YELLOW + "Warning: " + message + ANSI_RESET);
+            if (useColors) {
+                System.out.println(ANSIYellowColor + "Warning: " + message + ANSIReset);
+            }else{
+                System.out.println("Warning: " + message);
+            }
             lastMessageType = 3;
         }
     }
 
     public static void warning(Object message, String tag){
         if (warningMessages){
-            System.out.println(ANSI_YELLOW + "Warning." + tag + ": " + message + ANSI_RESET);
+            if (useColors){
+                System.out.println(ANSIYellowColor + "Warning." + tag + ": " + message + ANSIReset);
+            }else{
+                System.out.println("Warning." + tag + ": " + message);
+            }
+
             lastMessageType = 3;
         }
     }
 
     public static void error(Object message){
         if (errorMessages){
-            System.out.println(ANSI_RED + "Error: " + message + ANSI_RESET);
+            if (useColors){
+                System.out.println(ANSIRedColor + "Error: " + message + ANSIReset);
+            }else{
+                System.out.println("Error: " + message);
+            }
+
             lastMessageType = 4;
         }
     }
 
     public static void error(Object message, String tag){
         if (errorMessages){
-            System.out.println(ANSI_RED + "Error." + tag + ": " + message + ANSI_RESET);
+            if (useColors){
+                System.out.println(ANSIRedColor + "Error." + tag + ": " + message + ANSIReset);
+            }else{
+                System.out.println("Error." + tag + ": " + message);
+            }
+
             lastMessageType = 4;
         }
     }
@@ -127,22 +166,50 @@ public class Logger {
     public static void underMessage(String message){
         if (lastMessageType == 1){
             if (debugMessages){
-                BufferedReader reader = new BufferedReader(new StringReader(ANSI_CYAN + message + ANSI_RESET));
+
+                BufferedReader reader = null;
+                if (useColors){
+                    reader = new BufferedReader(new StringReader(ANSICyanColor + message + ANSIReset));
+                }else{
+                    reader = new BufferedReader(new StringReader(message));
+                }
+
                 printUnderTag(reader);
             }
         }else if (lastMessageType == 2){
             if (infoMessages){
-                BufferedReader reader = new BufferedReader(new StringReader(ANSI_WHITE + message + ANSI_RESET));
+
+                BufferedReader reader = null;
+                if (useColors){
+                    reader = new BufferedReader(new StringReader(ANSIWhiteColor + message + ANSIReset));
+                }else{
+                    reader = new BufferedReader(new StringReader(message));
+                }
+
                 printUnderTag(reader);
             }
         }else if (lastMessageType == 3){
             if (warningMessages){
-                BufferedReader reader = new BufferedReader(new StringReader(ANSI_YELLOW + message + ANSI_RESET));
+
+                BufferedReader reader = null;
+                if (useColors){
+                    reader = new BufferedReader(new StringReader(ANSIYellowColor + message + ANSIReset));
+                }else{
+                    reader = new BufferedReader(new StringReader(message));
+                }
+
                 printUnderTag(reader);
             }
         }else if (lastMessageType == 4){
             if (errorMessages){
-                BufferedReader reader = new BufferedReader(new StringReader(ANSI_RED + message + ANSI_RESET));
+
+                BufferedReader reader = null;
+                if (useColors){
+                    reader = new BufferedReader(new StringReader(ANSIRedColor + message + ANSIReset));
+                }else{
+                    reader = new BufferedReader(new StringReader(message));
+                }
+
                 printUnderTag(reader);
             }
         }
