6f5ac4e78d4d17f54761e2b05dfe99fbb4deca74
lokin135 <lokin135help@gmail.com>
2019-10-28 23:56:34 +0300

    Only GUI support

--- src/LokEngine/Application.java
+++ src/LokEngine/Application.java
@@ -171,7 +171,8 @@ public class Application {
 
             window.getCamera().updateAudioListener();
 
-            scene.update(applicationRuntime, window.getFrameBuilder().getScenePartsBuilder());
+            if (scene != null)
+                scene.update(applicationRuntime, window.getFrameBuilder().getScenePartsBuilder());
 
             window.update();
         }
@@ -202,6 +203,118 @@ public class Application {
 
     public void startConsole(){
         startConsole(true);
+    }
+
+    public void startGUIOnly(boolean windowFullscreen, boolean vSync, Vector2i windowResolution, String windowTitle) {
+        myThread = new Thread(new Runnable() {
+            @Override
+            public void run() {
+                try {
+                    synchronized (key) {
+                        if (!staticInited) {
+                            try {
+                                Prefs.init();
+                            } catch (Exception e) {
+                                Logger.warning("Fail load in prefs!", "LokEngine_start");
+                                Logger.printException(e);
+                            }
+
+                            Logger.debug("Init glfw", "LokEngine_start");
+                            glfwInit();
+                            staticInited = true;
+                        }
+                    }
+
+                    Logger.debug("Init window", "LokEngine_start");
+                    window = new Window();
+                    try {
+                        window.open(windowFullscreen, vSync, windowResolution);
+                        window.setTitle(windowTitle);
+                    } catch (Exception e) {
+                        Logger.error("Fail open window!", "LokEngine_start");
+                        Logger.printException(e);
+                    }
+                    try {
+                        SplashScreen.init(window);
+                    } catch (Exception e) {
+                        Logger.error("Fail init splash screen!", "LokEngine_start");
+                        Logger.printException(e);
+                    }
+
+                    SplashScreen.updateStatus(0.1f);
+                    Logger.debug("Init application runtime", "LokEngine_start");
+
+                    applicationRuntime = new ApplicationRuntime();
+                    applicationRuntime.init();
+
+                    SplashScreen.updateStatus(0.3f);
+                    Logger.debug("Init default font", "LokEngine_start");
+                    try {
+                        GraphicsEnvironment.getLocalGraphicsEnvironment().registerFont(Font.createFont(Font.TRUETYPE_FONT, this.getClass().getResourceAsStream("/resources/Fonts/Default.ttf")));
+                    } catch (FontFormatException | IOException e) {
+                        Logger.error("Fail register default font!", "LokEngine_start");
+                        Logger.printException(e);
+                    }
+
+                    SplashScreen.updateStatus(0.4f);
+                    Logger.debug("Init OpenAL", "LokEngine_start");
+
+                    String defaultDeviceName = ALC10.alcGetString(0, ALC10.ALC_DEFAULT_DEVICE_SPECIFIER);
+                    openALDevice = alcOpenDevice(defaultDeviceName);
+                    openALContext = ALC10.alcCreateContext(openALDevice, new int[]{0});
+                    alcMakeContextCurrent(openALContext);
+
+                    alcCapabilities = ALC.createCapabilities(openALDevice);
+                    alCapabilities = AL.createCapabilities(alcCapabilities);
+
+                    SplashScreen.updateStatus(0.5f);
+                    Logger.debug("Call user init method", "LokEngine_start");
+                    try {
+                        Init();
+                    } catch (Exception e) {
+                        Logger.warning("Fail user-init!", "LokEngine_start");
+                        Logger.printException(e);
+                    }
+
+                    SplashScreen.updateStatus(0.9f);
+                    Logger.debug("Turn in main while!", "LokEngine_start");
+                    System.gc();
+                    isRun = true;
+                } catch (Exception e) {
+                    Logger.error("Fail start engine!", "LokEngine_start");
+                    Logger.printException(e);
+                    System.exit(-1);
+                }
+
+                try {
+                    mainWhile();
+                } catch (Exception e) {
+                    Logger.error("Critical error in main while engine!", "LokEngine_runtime");
+                    Logger.printException(e);
+                    System.exit(-2);
+                }
+
+                Logger.debug("Exit from main while", "LokEngine_start");
+
+                try {
+                    Exit();
+                } catch (Exception e) {
+                    Logger.warning("Fail user-exit!", "LokEngine_postRuntime");
+                    Logger.printException(e);
+                }
+
+                alcDestroyContext(openALContext);
+                alcCloseDevice(openALDevice);
+
+                try {
+                    Prefs.save();
+                } catch (Exception e) {
+                    Logger.warning("Fail save prefs!", "LokEngine_postRuntime");
+                    Logger.printException(e);
+                }
+            }
+        });
+        myThread.start();
     }
 
     public void startConsole(boolean haveScene) {
