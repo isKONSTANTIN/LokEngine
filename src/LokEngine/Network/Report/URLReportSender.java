eaf18a1dc57ec85d1a26cc348c55e5e858ea1c2e
lokin135 <lokin135help@gmail.com>
2019-10-29 17:20:41 +0300

    URLReportSender: fixes and upgrade

--- src/LokEngine/Network/Report/URLReportSender.java
+++ src/LokEngine/Network/Report/URLReportSender.java
@@ -1,11 +1,15 @@
 package LokEngine.Network.Report;
 
 import com.google.gson.Gson;
+import org.apache.commons.io.IOUtils;
 
+import javax.net.ssl.HttpsURLConnection;
+import java.io.InputStream;
 import java.io.OutputStream;
 import java.net.HttpURLConnection;
 import java.net.Proxy;
 import java.net.URL;
+import java.nio.charset.Charset;
 
 public class URLReportSender {
 
@@ -29,8 +33,7 @@ public class URLReportSender {
         this(url, 5000);
     }
 
-    public void sendReport(Report report) throws Exception {
-        HttpURLConnection connection = (HttpURLConnection) (proxy != null ? url.openConnection(proxy) : url.openConnection());
+    protected String sendReport(Report report, HttpURLConnection connection) throws Exception {
         connection.setRequestMethod("POST");
         connection.setConnectTimeout(this.connectionTimeout);
 
@@ -40,6 +43,20 @@ public class URLReportSender {
         out.flush();
         out.close();
 
+        InputStream inputStream = connection.getInputStream();
+        String returnMessage = IOUtils.toString(inputStream, Charset.defaultCharset());
+
+        inputStream.close();
         connection.disconnect();
+
+        return returnMessage;
+    }
+
+    public String sendReport(Report report) throws Exception {
+        return sendReport(report, (HttpURLConnection) (proxy != null ? url.openConnection(proxy) : url.openConnection()));
+    }
+
+    public String sendReportOverHttps(Report report) throws Exception {
+        return sendReport(report, (HttpsURLConnection)(proxy != null ? url.openConnection(proxy) : url.openConnection()));
     }
 }
