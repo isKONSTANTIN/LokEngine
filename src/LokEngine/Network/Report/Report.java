738cc594c2afb2f032ae6715c57c9915c95ff4eb
lokin135 <lokin135help@gmail.com>
2019-10-29 18:44:18 +0300

    Report: add system info to HashMap

--- src/LokEngine/Network/Report/Report.java
+++ src/LokEngine/Network/Report/Report.java
@@ -5,7 +5,7 @@ import java.util.HashMap;
 public class Report {
     public String title;
     public String errorBody;
-    public String systemInfo;
+    public HashMap<String, String> systemInfo;
     public HashMap<String, String> otherInfo;
 
     public Report(String title, Exception error, HashMap<String, String> otherInfo){
@@ -30,16 +30,16 @@ public class Report {
         this(title,errorBody,new HashMap<>());
     }
 
-    protected static String getSystemInfo(){
-        StringBuilder stringBuilder = new StringBuilder();
+    protected static HashMap<String, String> getSystemInfo(){
+        HashMap<String, String> hashMap = new HashMap();
         long maxMemory = Runtime.getRuntime().maxMemory();
         boolean unlimitedMemory = maxMemory == Long.MAX_VALUE;
 
-        stringBuilder.append("AvailableProcessors: ").append(Runtime.getRuntime().availableProcessors()).append("\n");
-        stringBuilder.append("Free memory: ").append(Runtime.getRuntime().freeMemory() / 1000000f).append(" MB").append("\n");
-        stringBuilder.append("Max memory: ").append(unlimitedMemory ? "unlimited" : maxMemory / 1000000f).append(unlimitedMemory ? "" : " MB").append("\n");
-        stringBuilder.append("Total memory available to JVM: ").append(Runtime.getRuntime().totalMemory() / 1000000f).append(" MB");
+        hashMap.put("AvailableProcessors", String.valueOf(Runtime.getRuntime().availableProcessors()));
+        hashMap.put("Free memory", String.valueOf(Runtime.getRuntime().freeMemory() / 1000000f));
+        hashMap.put("Max memory", String.valueOf(unlimitedMemory ? "unlimited" : maxMemory / 1000000f));
+        hashMap.put("Total memory available to JVM", String.valueOf(Runtime.getRuntime().totalMemory() / 1000000f));
 
-        return stringBuilder.toString();
+        return hashMap;
     }
 }
