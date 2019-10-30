5309bbe6af28f13518b3ab7b6ae9e904856e60bc
lokin135 <lokin135help@gmail.com>
2019-10-30 20:40:53 +0300

    Console work mode: fix thread

--- src/LokEngine/Application.java
+++ src/LokEngine/Application.java
@@ -280,59 +280,62 @@ public class Application {
     }
 
     public void startConsole(boolean haveScene) {
-        try {
-            Prefs.init();
-        } catch (Exception e) {
-            Logger.warning("Fail load in prefs!", "LokEngine_start");
-            Logger.printException(e);
-        }
-
-        Logger.debug("Init application runtime", "LokEngine_start");
-
-        applicationRuntime = new ApplicationRuntime();
-        applicationRuntime.init();
-
-        try {
-            if (haveScene){
-                Logger.debug("Init Scene", "LokEngine_start");
-                scene = new Scene();
-            }
-            Logger.debug("Call user init method", "LokEngine_start");
+        myThread = new Thread(() -> {
             try {
-                Init();
+                Prefs.init();
             } catch (Exception e) {
-                Logger.warning("Fail user-init!", "LokEngine_start");
+                Logger.warning("Fail load in prefs!", "LokEngine_start");
                 Logger.printException(e);
             }
-            Logger.debug("Turn in main while!", "LokEngine_start");
-            System.gc();
-            isRun = true;
-        } catch (Exception e) {
-            Logger.error("Fail start engine!", "LokEngine_start");
-            Logger.printException(e);
-            System.exit(-1);
-        }
-        try {
-            consoleMainWhile();
-        } catch (Exception e) {
-            Logger.error("Critical error in main while engine!", "LokEngine_runtime");
-            Logger.printException(e);
-            System.exit(-2);
-        }
 
-        try {
-            Exit();
-        } catch (Exception e) {
-            Logger.warning("Fail user-exit!", "LokEngine_postRuntime");
-            Logger.printException(e);
-        }
+            Logger.debug("Init application runtime", "LokEngine_start");
 
-        try {
-            Prefs.save();
-        } catch (Exception e) {
-            Logger.warning("Fail save prefs!", "LokEngine_postRuntime");
-            Logger.printException(e);
-        }
+            applicationRuntime = new ApplicationRuntime();
+            applicationRuntime.init();
+
+            try {
+                if (haveScene) {
+                    Logger.debug("Init Scene", "LokEngine_start");
+                    scene = new Scene();
+                }
+                Logger.debug("Call user init method", "LokEngine_start");
+                try {
+                    Init();
+                } catch (Exception e) {
+                    Logger.warning("Fail user-init!", "LokEngine_start");
+                    Logger.printException(e);
+                }
+                Logger.debug("Turn in main while!", "LokEngine_start");
+                System.gc();
+                isRun = true;
+            } catch (Exception e) {
+                Logger.error("Fail start engine!", "LokEngine_start");
+                Logger.printException(e);
+                System.exit(-1);
+            }
+            try {
+                consoleMainWhile();
+            } catch (Exception e) {
+                Logger.error("Critical error in main while engine!", "LokEngine_runtime");
+                Logger.printException(e);
+                System.exit(-2);
+            }
+
+            try {
+                Exit();
+            } catch (Exception e) {
+                Logger.warning("Fail user-exit!", "LokEngine_postRuntime");
+                Logger.printException(e);
+            }
+
+            try {
+                Prefs.save();
+            } catch (Exception e) {
+                Logger.warning("Fail save prefs!", "LokEngine_postRuntime");
+                Logger.printException(e);
+            }
+        });
+        myThread.start();
     }
 
     public void close() {
