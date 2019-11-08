b32718387ea9371dbe75552c26b95c2aff218883
lokin135 <lokin135help@gmail.com>
2019-11-08 22:03:25 +0300

    New GUI Object: Slider

---
+++ src/ru/lokinCompany/lokEngine/GUI/GUIObjects/GUISlider/GUISliderColorShader.java
@@ -0,0 +1,20 @@
+package ru.lokinCompany.lokEngine.GUI.GUIObjects.GUISlider;
+
+import ru.lokinCompany.lokEngine.Tools.Utilities.Color.Color;
+import ru.lokinCompany.lokEngine.Tools.Utilities.Color.Colors;
+import ru.lokinCompany.lokEngine.Tools.Utilities.Color.SimpleColorShader;
+import ru.lokinCompany.lokEngine.Tools.Utilities.Vector2i;
+
+public interface GUISliderColorShader extends SimpleColorShader {
+
+    Color getColor(float sliderValue);
+
+    @Override
+    default Color getColor(Object input) {
+        try {
+            return getColor((float)input);
+        }catch (Exception e){
+            return Colors.black();
+        }
+    }
+}
