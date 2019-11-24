fd38ac984c17ce540128d1305bfcff8c260748cd
lokin135 <lokin135help@gmail.com>
2019-11-24 23:13:14 +0300

    Tests: add example program

--- src/ru/lokinCompany/lokEngine/Tests/ExampleProgram.java
+++ src/ru/lokinCompany/lokEngine/Tests/ExampleProgram.java
@@ -1,7 +1,21 @@
 package ru.lokinCompany.lokEngine.Tests;
 
 import ru.lokinCompany.lokEngine.Applications.ApplicationDefault;
+import ru.lokinCompany.lokEngine.GUI.AdditionalObjects.GUIPositions.GUIPosition;
+import ru.lokinCompany.lokEngine.GUI.GUIObjects.GUIText;
+import ru.lokinCompany.lokEngine.Render.Window.Window;
+import ru.lokinCompany.lokEngine.Render.Window.WindowEvent;
+import ru.lokinCompany.lokEngine.Tools.Timer;
+import ru.lokinCompany.lokEngine.Tools.Utilities.Color.Colors;
+import ru.lokinCompany.lokEngine.Tools.Utilities.Vector2i;
 
 public class ExampleProgram extends ApplicationDefault {
+    public static void main(String[] args) { new ExampleProgram().start(false,true); }
 
+    @Override
+    protected void initEvent() {
+        window.setCloseEvent((window, args) -> close());
+        window.getFrameBuilder().backgroundColor = Colors.engineBackgroundColor();
+        window.getCanvas().addObject(new GUIText(new Vector2i(),"It's simple program on LokEngine!"), GUIPosition.Center);
+    }
 }
