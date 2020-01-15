b7689dfb6ba32202a865f6761ebd18d33734a366
lokin135 <lokin135help@gmail.com>
2020-01-15 22:29:15 +0300

    ShapeCreator: fixes

--- src/ru/lokincompany/lokengine/sceneenvironment/defaultenvironment/components/componentstools/ShapeCreator.java
+++ src/ru/lokincompany/lokengine/sceneenvironment/defaultenvironment/components/componentstools/ShapeCreator.java
@@ -1,11 +1,11 @@
-package ru.lokincompany.lokengine.sceneenvironment.components.componentstools;
+package ru.lokincompany.lokengine.sceneenvironment.defaultenvironment.components.componentstools;
 
 import org.jbox2d.collision.shapes.PolygonShape;
 import org.jbox2d.common.Vec2;
 import org.lwjgl.util.vector.Vector2f;
-import ru.lokincompany.lokengine.sceneenvironment.components.additionalobjects.rigidbody.shapes.ArbitraryShape;
-import ru.lokincompany.lokengine.sceneenvironment.components.additionalobjects.rigidbody.shapes.BoxShape;
-import ru.lokincompany.lokengine.sceneenvironment.components.additionalobjects.rigidbody.shapes.CircleShape;
+import ru.lokincompany.lokengine.sceneenvironment.defaultenvironment.components.additionalobjects.rigidbody.shapes.ArbitraryShape;
+import ru.lokincompany.lokengine.sceneenvironment.defaultenvironment.components.additionalobjects.rigidbody.shapes.BoxShape;
+import ru.lokincompany.lokengine.sceneenvironment.defaultenvironment.components.additionalobjects.rigidbody.shapes.CircleShape;
 
 public class ShapeCreator {
 
@@ -15,7 +15,7 @@ public class ShapeCreator {
         Vec2[] b2Points = new Vec2[length];
 
         for (int i = 0; i < length; i++) {
-            b2Points[i] = new Vec2((collidePoints[i].x - 3.55f) * 0.005f, (collidePoints[i].y - 3.55f) * 0.005f);
+            b2Points[i] = new Vec2(collidePoints[i].x * 0.005f, collidePoints[i].y * 0.005f);
         }
 
         polyShape.set(b2Points, length);
@@ -25,14 +25,14 @@ public class ShapeCreator {
 
     public static BoxShape CreateBoxShape(Vector2f collideSize) {
         PolygonShape polyShape = new PolygonShape();
-        polyShape.setAsBox((collideSize.x - 3.55f) * 0.005f, (collideSize.y - 3.55f) * 0.005f);
+        polyShape.setAsBox(collideSize.x * 0.005f, collideSize.y * 0.005f);
 
         return new BoxShape(collideSize, polyShape);
     }
 
     public static CircleShape CreateCircleShape(float radius) {
         org.jbox2d.collision.shapes.CircleShape shape = new org.jbox2d.collision.shapes.CircleShape();
-        shape.m_radius = (radius - 3.55f) * 0.005f;
+        shape.m_radius = radius * 0.005f;
 
         return new CircleShape(radius, shape);
     }
