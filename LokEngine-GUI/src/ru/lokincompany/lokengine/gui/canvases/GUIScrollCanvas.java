0bc776d7ff29b0c56dbed86264a3b8a6d4a48ca3
lokin135 <lokin135help@gmail.com>
2020-02-13 14:55:52 +0300

    GUIScrollCanvas: added smooth scroll

--- LokEngine-GUI/src/ru/lokincompany/lokengine/gui/canvases/GUIScrollCanvas.java
+++ LokEngine-GUI/src/ru/lokincompany/lokengine/gui/canvases/GUIScrollCanvas.java
@@ -1,6 +1,7 @@
 package ru.lokincompany.lokengine.gui.canvases;
 
 import org.lwjgl.util.vector.Vector2f;
+import org.lwjgl.util.vector.Vector4f;
 import ru.lokincompany.lokengine.gui.additionalobjects.GUIObjectProperties;
 import ru.lokincompany.lokengine.render.frame.PartsBuilder;
 import ru.lokincompany.lokengine.tools.vectori.Vector2i;
@@ -9,8 +10,11 @@ public class GUIScrollCanvas extends GUICanvas {
 
     public Vector2i maxOffset;
     public Vector2i minOffset;
+    public float scrollStrength = 2f;
+    public float scrollDeceleration = 1.1f;
 
-    public float scrollStrength = 15;
+    protected Vector2f scrollImpulse = new Vector2f();
+    protected Vector2f scrollPosition = new Vector2f();
 
     public GUIScrollCanvas(Vector2i position, Vector2i size, Vector2i maxOffset, Vector2i minOffset) {
         super(position, size);
@@ -29,14 +33,8 @@ public class GUIScrollCanvas extends GUICanvas {
     protected void updateScroll(GUIObjectProperties parentProperties) {
         Vector2f mouseScroll = parentProperties.mouseRaycastStatus.mouse.getMouseScroll();
 
-        int Xmin = (int) (minOffset != null ? Math.max(framePart.viewOffset.x + mouseScroll.x * scrollStrength, minOffset.x) : framePart.viewOffset.x + mouseScroll.x * 10);
-        int Xmax = maxOffset != null ? Math.min(Xmin, maxOffset.x) : Xmin;
-
-        int Ymin = (int) (minOffset != null ? Math.max(framePart.viewOffset.y + mouseScroll.y * scrollStrength, minOffset.y) : framePart.viewOffset.y + mouseScroll.y * 10);
-        int Ymax = maxOffset != null ? Math.min(Ymin, maxOffset.y) : Ymin;
-
-        framePart.viewOffset.x = Xmax;
-        framePart.viewOffset.y = Ymax;
+        scrollImpulse.x += mouseScroll.x * scrollStrength;
+        scrollImpulse.y += mouseScroll.y * scrollStrength;
     }
 
     @Override
@@ -45,6 +43,24 @@ public class GUIScrollCanvas extends GUICanvas {
 
         if (properties.mouseRaycastStatus.mouse.inField(properties.globalPosition, size))
             updateScroll(parentProperties);
+
+        int Xmin = (int) (minOffset != null ? Math.max(scrollPosition.x + scrollImpulse.x, minOffset.x) : scrollPosition.x + scrollImpulse.x);
+        int Xmax = maxOffset != null ? Math.min(Xmin, maxOffset.x) : Xmin;
+
+        int Ymin = (int) (minOffset != null ? Math.max(scrollPosition.y + scrollImpulse.y, minOffset.y) : scrollPosition.y + scrollImpulse.y);
+        int Ymax = maxOffset != null ? Math.min(Ymin, maxOffset.y) : Ymin;
+
+        scrollPosition.x = Xmax;
+        scrollPosition.y = Ymax;
+
+        scrollImpulse.x /= scrollDeceleration;
+        scrollImpulse.y /= scrollDeceleration;
+
+        if (Math.abs(scrollImpulse.x) <= 0.2f) scrollImpulse.x = 0;
+        if (Math.abs(scrollImpulse.y) <= 0.2f) scrollImpulse.y = 0;
+
+        framePart.viewOffset.x = (int)scrollPosition.x;
+        framePart.viewOffset.y = (int)scrollPosition.y;
 
         properties.globalPosition.x += framePart.viewOffset.x;
         properties.globalPosition.y += framePart.viewOffset.y;
