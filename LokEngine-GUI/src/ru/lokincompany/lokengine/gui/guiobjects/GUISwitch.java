8cc7310cbb7475b4631752596d7677dcdbc3244d
lokin135 <lokin135help@gmail.com>
2020-02-15 14:53:25 +0300

    GUISwitch: added support text

--- LokEngine-GUI/src/ru/lokincompany/lokengine/gui/guiobjects/GUISwitch.java
+++ LokEngine-GUI/src/ru/lokincompany/lokengine/gui/guiobjects/GUISwitch.java
@@ -10,34 +10,36 @@ import ru.lokincompany.lokengine.tools.vectori.Vector2i;
 public class GUISwitch extends GUIObject {
     public GUILocationAlgorithm switchSizeAlgorithm;
     protected GUISwitchFramePart framePart;
+    protected GUIText text;
 
     public GUISwitch() {
-        super(new Vector2i(), new Vector2i(25,10));
+        super(new Vector2i(), new Vector2i(25,15));
         framePart = new GUISwitchFramePart(position, size);
-        updateHeadPosition();
-        updateHeadSize();
-
-
     }
 
-    public void setColorHead(Color colorHead){
+    public GUISwitch setColorHead(Color colorHead){
         framePart.colorHead = colorHead;
+        return this;
     }
 
-    public void setColorBackground(Color colorBackground){
-        framePart.colorBackground= colorBackground;
+    public GUISwitch setColorBackground(Color colorBackground){
+        framePart.colorBackground = colorBackground;
+        return this;
     }
 
-    public void setColorFill(Color colorFill){
+    public GUISwitch setColorFill(Color colorFill){
         framePart.colorFill = colorFill;
+        return this;
     }
 
-    protected void updateHeadPosition() {
-        framePart.headPosition = new Vector2i(framePart.status ? (position.x + size.x - framePart.headSize.x) : position.x, position.y);
-    }
-
-    protected void updateHeadSize() {
-        framePart.headSize = switchSizeAlgorithm != null ? switchSizeAlgorithm.calculate(this) : new Vector2i(size.x / 3, size.y);
+    public GUISwitch setText(GUIText text){
+        this.text = text;
+        text.setPosition(object -> new Vector2i(position.x + size.x + 2, position.y));
+        setSize(new Vector2i(
+                (int)(text.framePart.font.getFontHeight() * (5f / 3f)),
+                text.framePart.font.getFontHeight()
+                ));
+        return this;
     }
 
     public void switchStatus() {
@@ -48,16 +50,15 @@ public class GUISwitch extends GUIObject {
         return framePart.status;
     }
 
-    public void setStatus(boolean status) {
+    public GUISwitch setStatus(boolean status) {
         framePart.status = status;
-        updateHeadPosition();
+        return this;
     }
 
     @Override
     public GUISwitch setPosition(Vector2i position) {
         super.setPosition(position);
         framePart.position = position;
-        updateHeadPosition();
         return this;
     }
 
@@ -65,7 +66,10 @@ public class GUISwitch extends GUIObject {
     public GUISwitch setSize(Vector2i size) {
         super.setSize(size);
         framePart.size = size;
-        updateHeadSize();
+
+        framePart.headSize.x = (int)(size.x / 2.5f);
+        framePart.headSize.y = size.y;
+
         return this;
     }
 
@@ -73,8 +77,22 @@ public class GUISwitch extends GUIObject {
     public void update(PartsBuilder partsBuilder, GUIObjectProperties parentProperties) {
         super.update(partsBuilder, parentProperties);
 
+        if (text != null)
+            text.update(partsBuilder, parentProperties);
+
         if (properties.mouseRaycastStatus.mouse.inField(new Vector2i(parentProperties.globalPosition.x + framePart.headPosition.x, parentProperties.globalPosition.y + framePart.headPosition.y), framePart.headSize) && properties.mouseRaycastStatus.mouse.getPressedStatus() && !properties.mouseRaycastStatus.lastFramePressed)
             switchStatus();
+
+        if (framePart.status && framePart.animationStatus < 1) {
+            framePart.animationStatus += 0.1f;
+        }else if (!framePart.status && framePart.animationStatus > 0)
+            framePart.animationStatus -= 0.1f;
+
+        framePart.headPosition.x = (int)(position.x + (size.x - framePart.headSize.x) * framePart.animationStatus);
+        framePart.headPosition.y = position.y;
+
+        if (switchSizeAlgorithm != null)
+            framePart.headSize = switchSizeAlgorithm.calculate(this);
 
         partsBuilder.addPart(framePart);
     }
--- LokEngine-GUI/src/ru/lokincompany/lokengine/render/frame/frameparts/gui/GUISwitchFramePart.java
+++ LokEngine-GUI/src/ru/lokincompany/lokengine/render/frame/frameparts/gui/GUISwitchFramePart.java
@@ -11,22 +11,27 @@ import ru.lokincompany.lokengine.tools.vectori.Vector2i;
 import static org.lwjgl.opengl.GL11.*;
 
 public class GUISwitchFramePart extends FramePart {
-
     public Color colorHead;
     public Color colorBackground;
     public Color colorFill;
 
     public Vector2i position;
     public Vector2i size;
-    public Vector2i headPosition = new Vector2i();
+    public Vector2i headPosition;
     public Vector2i headSize = new Vector2i();
 
+    public float animationStatus;
     public boolean status;
 
     public GUISwitchFramePart(Vector2i position, Vector2i size, Color colorHead, Color colorBackground, Color colorFill) {
         super(FramePartType.GUI);
         this.position = position;
+        headPosition = new Vector2i(position.x, position.y);
+
         this.size = size;
+        headSize.x = (int)(size.x / 2.5f);
+        headSize.y = size.y;
+
         this.colorHead = colorHead;
         this.colorBackground = colorBackground;
         this.colorFill = colorFill;
@@ -42,12 +47,11 @@ public class GUISwitchFramePart extends FramePart {
 
     @Override
     public void partRender(RenderProperties renderProperties) {
-        if (status)
-            glColor4f(colorFill.red, colorFill.green, colorFill.blue, colorFill.alpha);
-        else
-            glColor4f(colorBackground.red, colorBackground.green, colorBackground.blue, colorBackground.alpha);
-
+        glColor4f(colorBackground.red, colorBackground.green, colorBackground.blue, colorBackground.alpha);
         OpenGLFastTools.drawSquare(position, size);
+
+        glColor4f(colorFill.red, colorFill.green, colorFill.blue, colorFill.alpha);
+        OpenGLFastTools.drawSquare(position, new Vector2i((int)(size.x * animationStatus), size.y));
 
         glColor4f(colorHead.red, colorHead.green, colorHead.blue, colorHead.alpha);
         OpenGLFastTools.drawSquare(headPosition, headSize);
